12b2611fec0b077345da8ca7e631dc9a055dc461
CMorar143 <cian@morar.org>
2020-05-08 18:38:10 +0100

    bad guys find each other correctly

--- Serenity_Space_Battle/Assets/Scripts/OffsetPursue.cs
+++ Serenity_Space_Battle/Assets/Scripts/OffsetPursue.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class OffsetPursue : MonoBehaviour
 {
 	public GameObject badMain;
+	private Vector3 offset;
+	private Vector3 target;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+		offset = transform.position - badMain.transform.position;
+		offset = Quaternion.Inverse(badMain.transform.rotation) * offset;
+	}
 
     // Update is called once per frame
     void Update()
