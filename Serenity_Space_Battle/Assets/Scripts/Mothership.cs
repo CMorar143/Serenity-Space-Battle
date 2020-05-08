e40abaf951f87c934eb0bddfc99ada4dea3c3032
CMorar143 <cian@morar.org>
2020-05-08 14:23:51 +0100

    bad guys find each other correctly

--- Serenity_Space_Battle/Assets/Scripts/Mothership.cs
+++ Serenity_Space_Battle/Assets/Scripts/Mothership.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Mothership : MonoBehaviour
 {
-	private List<GameObject> antagonists;
+	public GameObject[] antagonists;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		
+		antagonists = GameObject.FindGameObjectsWithTag("BadGuy");
+
+		foreach (GameObject badGuy in antagonists)
+		{
+			badGuy.AddComponent<OffsetPursue>().badMain = this.gameObject;
+		}
 	}
 
 	// Update is called once per frame
--- Serenity_Space_Battle/Assets/Scripts/OffsetPursue.cs
+++ Serenity_Space_Battle/Assets/Scripts/OffsetPursue.cs
@@ -6,7 +6,6 @@ public class OffsetPursue : MonoBehaviour
 {
 	public GameObject badMain;
 
-
     // Start is called before the first frame update
     void Start()
     {
