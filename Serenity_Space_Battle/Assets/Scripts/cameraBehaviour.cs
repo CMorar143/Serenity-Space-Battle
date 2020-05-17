12eef5293d9b034497d997c41207ead2f97a08e2
CMorar143 <cian@morar.org>
2020-05-17 14:07:03 +0100

    lerping camera

--- Serenity_Space_Battle/Assets/Scripts/cameraBehaviour.cs
+++ Serenity_Space_Battle/Assets/Scripts/cameraBehaviour.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class cameraBehaviour : MonoBehaviour
 {
-	public GameObject cam;
+	private GameObject cam;
+	public float speed = 1.0f;
+	private float journeyLength;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+		cam = GameObject.FindGameObjectWithTag("MainCamera");
+		journeyLength = Vector3.Distance(cam.transform.position, this.transform.position);
     }
+
+	IEnumerator LerpCamera(Vector3 source, Vector3 target, float overTime)
+	{
+		yield return new WaitForSecondsRealtime(5f);
+		float startTime = Time.time;
+
+		while (Time.time < startTime + overTime)
+		{
+			cam.transform.position = Vector3.Lerp(source, target, (Time.time - startTime) / overTime);
+			yield return null;
+		}
+		cam.transform.position = target;
+	}
+
+	//private void LerpCamera()
+	//{
+		
+	//}
 
 	private void OnTriggerEnter(Collider other)
 	{
-		
+		if (other.tag == "Mothership")
+		{
+			StartCoroutine(LerpCamera(cam.transform.position, this.transform.position, 3.0f));
+		}
 	}
 
 	// Update is called once per frame
